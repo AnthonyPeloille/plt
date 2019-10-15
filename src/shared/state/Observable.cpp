046c6bed01cacc42581300cf4a3e6d7b2ee973f3
alex <alexandre.genot@ensea.fr>
2019-10-15 15:31:20 +0200

    Updated observer and added clone methods to Dia diagram

--- src/shared/state/Observable.cpp
+++ src/shared/state/Observable.cpp
@@ -1,14 +1,19 @@
 
 
 #include "Observable.h"
+#include <algorithm>
 
-void state::Observable::registerObserver(Observer *observer) {
-    this->observer.push_back(observer);
+void const state::Observable::registerObserver(Observer *o) {
+    this->observer.push_back(o);
 }
 
-void state::Observable::notifyObserver(const StateEvent &e) {
+void const state::Observable::notifyObserver(const Event &e) {
     for (auto o : observer)
         o->stateChanged(e);
+}
+
+void const state::Observable::unregisterObserver(state::Observer *o) {
+    observer.erase(find(observer.cbegin(),observer.cend(),o));
 }
 
 state::Observable::~Observable() = default;
--- src/shared/state/StateEvent.cpp
+++ src/shared/state/StateEvent.cpp
@@ -2,11 +2,15 @@
 
 #include "StateEvent.h"
 
-state::StateEvent::StateEvent(StateEventId stateEvent) {
-    this->stateEventId = stateEvent;
+state::StateEvent::StateEvent(StateEventId se) {
+    this->stateEvent = se;
 }
 
 const state::EventTypeId state::StateEvent::getEventType() {
     return EVENT_STATE;
+}
+
+void stateChanged(const state::Event& event){
+
 }
 
