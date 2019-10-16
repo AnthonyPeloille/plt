ef6edcb148dfa23c2aa73f6c38734f696e6c98fc
Anthony Peloille <anthony.peloille@ensea.fr>
2019-10-16 15:29:29 +0200

    Exemple removed

--- test/shared/test_shared_dummy.cpp
+++ test/shared/test_shared_dummy.cpp
@@ -1,8 +1,6 @@
 
 #include <boost/test/unit_test.hpp>
 
-#include "../../src/shared/state/Exemple.h"
-
 #include "../../src/shared/state.h"
 
 using namespace ::state;
@@ -11,24 +9,6 @@ BOOST_AUTO_TEST_CASE(TestStaticAssert)
 {
   BOOST_CHECK(1);
 }
-
-BOOST_AUTO_TEST_CASE(TestExemple)
-{
-  {
-    Exemple ex {};
-    BOOST_CHECK_EQUAL(ex.x, 0);
-    ex.setX(21);
-    BOOST_CHECK_EQUAL(ex.x, 21);
-  }
-
-  {
-    Exemple ex {};
-    ex.setX(21);
-    BOOST_CHECK_LE(ex.x, 32); // Less than equal
-    BOOST_CHECK_GT(ex.x, 11); // Greater than equl
-  }
-}
-
 
 BOOST_AUTO_TEST_CASE(TestDoor)
 {
