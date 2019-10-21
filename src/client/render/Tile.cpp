61a1ba5896edd2cfdcd7da3c49ba1c550b900020
Anthony Peloille <anthony.peloille@ensea.fr>
2019-10-21 09:01:03 +0200

    GridTileSet, Tile and TileSet updated

--- src/client/render/Tile.cpp
+++ src/client/render/Tile.cpp
@@ -1,4 +1,46 @@
-//
-// Created by alex on 18/10/2019.
-//
 
+#include "Tile.h"
+
+render::Tile::Tile() {
+    this-> width = 0;
+    this->height = 0;
+}
+
+render::Tile::Tile(int x, int y, int w, int h) {
+    this->x = x;
+    this->y = y;
+    this->width = w;
+    this->height = h;
+}
+
+int render::Tile::getX() const {
+    return x;
+}
+
+void render::Tile::setX(int x) {
+    this->x = x;
+}
+
+int render::Tile::getY() const {
+    return y;
+}
+
+void render::Tile::setY(int y) {
+    this->y = y;
+}
+
+int render::Tile::getWidth() const {
+    return width;
+}
+
+void render::Tile::setWidth(int width) {
+    this->width = width;
+}
+
+int render::Tile::getHeight() const {
+    return height;
+}
+
+void render::Tile::setHeight(int height) {
+    this->height = height;
+}
