f5614de032edd551ca56ad9921659f825e0d59c7
Anthony Peloille <anthony.peloille@ensea.fr>
2019-10-22 10:41:01 +0200

    issue with Scene.cpp

--- src/client/render/Scene.cpp
+++ src/client/render/Scene.cpp
@@ -1,4 +1,22 @@
-//
-// Created by anthony on 22/10/2019.
-//
+#include "Scene.h"
 
+render::Scene::Scene(const state::State &state) :state(state),stateLayer(this->state),gridLayer(this->state.getGrid()), charsLayer(this->state.getChars()) {
+
+}
+
+const size_t render::Scene::getWidth() {
+    return 400;
+}
+
+const size_t render::Scene::getHeight() {
+    return 400;
+}
+
+void render::Scene::stateChanged(const state::Event &event) {
+
+}
+
+void render::Scene::draw(sf::RenderWindow &window) {
+    this->gridLayer.initSurface();
+    window.draw(*this->gridLayer.getSurface());
+}
