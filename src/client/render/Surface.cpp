12ded7ae2d09f6d75ec43655232f5e74fa1979dc
Anthony Peloille <anthony.peloille@ensea.fr>
2019-10-22 09:42:50 +0200

    Surface.cpp one tile render

--- src/client/render/Surface.cpp
+++ src/client/render/Surface.cpp
@@ -12,49 +12,24 @@ void render::Surface::initQuads(int count) {
 }
 
 void render::Surface::setSprite(const Tile &tex) {
-    sf::Vertex quad[4];
+    sf::Vertex* quad = &quads[0];
 
-    //quad[0].position = sf::Vector2f(16,16);
-   // quad[1].position = sf::Vector2f(16,16+tex.getY());
-   // quad[2].position = sf::Vector2f(16+tex.getX(),16+tex.getY());
-//    quad[3].position = sf::Vector2f(16+tex.getX(),16);
-//
-//    quad[0].color = sf::Color::Red;
-//    quad[1].color = sf::Color::Blue;
-//    quad[2].color = sf::Color::Green;
-//    quad[2].color = sf::Color::Yellow;
-//
-//    //quad[0].texCoords = sf::Vector2f(16,16);
-//    //quad[1].texCoords = sf::Vector2f(16,16+tex.getY());
-//    //quad[2].texCoords = sf::Vector2f(16+tex.getX(),16+tex.getY());
-//    //quad[3].texCoords = sf::Vector2f(16+tex.getX(),16);
-//
-//    this->quads.append(*quad);
-    sf::Vertex triangle[4];
+    quad[0].position = sf::Vector2f(16,16);
+    quad[1].position = sf::Vector2f(16,16+tex.getY());
+    quad[2].position = sf::Vector2f(16+tex.getX(),16+tex.getY());
+    quad[3].position = sf::Vector2f(16+tex.getX(),16);
 
-// on définit la position des sommets du triangle
-    triangle[0].position = sf::Vector2f(10.f, 10.f);
-    triangle[1].position = sf::Vector2f(100.f, 10.f);
-    triangle[2].position = sf::Vector2f(100.f, 100.f);
-    triangle[3].position = sf::Vector2f(10.f, 100.f);
-// on définit la couleur des sommets du triangle
-    triangle[0].color = sf::Color::Red;
-    triangle[1].color = sf::Color::Blue;
-    triangle[2].color = sf::Color::Green;
-    triangle[3].color = sf::Color::Yellow;
-    this->quads[0] = *triangle;
+    quad[0].texCoords = sf::Vector2f(16,16);
+    quad[1].texCoords = sf::Vector2f(16,16+tex.getY());
+    quad[2].texCoords = sf::Vector2f(16+tex.getX(),16+tex.getY());
+    quad[3].texCoords = sf::Vector2f(16+tex.getX(),16);
+
 }
 
 void render::Surface::draw(sf::RenderTarget &target, sf::RenderStates states) const{
 
-    //states.texture = &texture;
-    //target.draw(quads, states);
+    states.texture = &texture;
 
-    //sf::Sprite sprite;
-    //sprite.setTexture(texture);
-    //sprite.setTextureRect(sf::IntRect(16, 16, 16, 16));
-    //sprite.setPosition(100, 25);
-
-    target.draw(quads);
+    target.draw(quads,states);
 
 }
