36cb14d09f78bdad28e6646a5d50988cbaa10471
alex <alexandre.genot@ensea.fr>
2019-11-08 09:30:28 +0100

    Implemented engine.cpp

--- src/shared/engine/Engine.cpp
+++ src/shared/engine/Engine.cpp
@@ -2,12 +2,15 @@
 #include "Engine.h"
 
 engine::Engine::Engine() {
-
+    this->currentState = state::State();
+    std::string map = "../res/map.txt";
+    currentState.initFloor(map,80,45);
+    std::string wall = "../res/wall.txt";
+    currentState.initWall(wall,80,45);
+    currentState.initChars(wall,80,45);
 }
 
-engine::Engine::~Engine() {
-
-}
+engine::Engine::~Engine() = default;
 
 const state::State &engine::Engine::getState() const {
     return this->currentState;
@@ -18,9 +21,18 @@ void engine::Engine::addPassiveCommands() {
 }
 
 void engine::Engine::addCommand(int priority, Command *cmd) {
-
+    currentCommands[priority] = std::unique_ptr<Command>(cmd);
 }
 
 void engine::Engine::update() {
 
+    std::map<int, std::unique_ptr<Command>>::iterator it;
+
+    for(size_t i=0;i<currentCommands.size();i++){
+        currentCommands[i]->execute(currentState);
+    }
+    for(it=currentCommands.begin();it!=currentCommands.end();it++){
+        currentCommands.erase(it);
+    }
+    currentCommands.clear();
 }
